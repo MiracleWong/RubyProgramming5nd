4eeb54dff430137d40bd401d18d261fa6a9fdeb1
MiracleWong <cfwr1991@126.com>
2017-07-10 09:59:52 +0800

    Add the Chapter18

---
+++ Chapter18/File.rb
@@ -0,0 +1,58 @@
+require "fileutils"
+File.rename("after.txt", "after.txt")
+
+# 将文件移动到已存在的目录下，目录不存在则程序会产生错误
+# File.rename("after.txt", "backup/after.txt")
+
+## 如果文件不存在，或者诶呦适当的文件操作权限，则文件操作失败，程序抛出异常
+
+
+## 利用File.open方法与write方法的组合来实现文件复制
+def copy(from, to)
+  File.open(from) do |input|
+    File.open(to, "w") do |output|
+      output.write(input.read)
+    end
+  end
+end
+
+## 使用FileUtils进行复制和移动
+# FileUtils.cp ("data.txt", "/backup/data.txt")
+# FileUtils.mv ("after.txt", "/backup/after.txt")
+
+
+## 删除文件
+# File.delete("after1.txt")
+# File.unlink("fool1.txt")
+
+
+
+## 目录的操作
+p Dir.pwd
+Dir.chdir("backup")
+p Dir.pwd
+Dir.chdir("/etc")
+p Dir.pwd
+Dir.chdir("/Users/miraclewong/github/ruby/Chapter18")
+p Dir.pwd
+
+## 当前目录下的文件，我们可以通过制定文件名直接打开，但如果变更了当前目录，则还需要指定目录名
+io = File.open("File.rb")
+io.close
+Dir.chdir("../Chapter17")
+p Dir.pwd
+io = File.open("data.txt")
+io.close
+
+## 打开文件，读取内容，关闭
+dir = Dir.open("/usr/bin")
+while name = dir.read
+  p name
+end
+dir.close
+
+Dir.open("/usr/bin") do |dir|
+  dir.each do |name|
+    p name
+  end
+end
